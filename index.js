cb331e495d97e88848af3d3724554c32f1009638
pankajkekane <kekanepankaj@gmail.com>
2021-06-27 15:19:39 +0530

    dom manipulation

---
+++ index.js
@@ -0,0 +1,12 @@
+const saveChanges = () => {
+    const taskData = {
+        id: `${Date.now()}`,   //unique no for card id
+        imageUrl: document.getElementById("imageurl").value,
+        taskTitle: document.getElementById("tasktitle").value,
+        taskType: document.getElementById("tasktype").value,
+        taskDescription: document.getElementById("taskdescription").value,
+
+    };
+
+    console.log(taskData);
+};
