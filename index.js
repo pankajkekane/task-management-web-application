8af0bc7688735aa5a939ca6363737eb607428de5
pankajkekane <kekanepankaj@gmail.com>
2021-06-27 16:04:18 +0530

    jawa script modification

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+const taskContainer = document.querySelector(".task__container");
+console.log(taskContainer);
+
+const newCard = ({id, imageUrl, taskTitle, taskType, taskDescription,}) => 
+` <div class="col-md-6 col-lg-4" id=${id}>
+<div class="card text-center">
+  <div class="card-header d-flex justify-content-end gap-2">
+    <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
+<button type="button" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+  </div>
+  <img src=${imageUrl} class="card-img-top" alt="card image">
+  <div class="card-body">
+    <h5 class="card-title">${taskTitle}</h5>
+    <p class="card-text">${taskDescription}</p>
+    <h5><span class="badge bg-primary">${taskType}</span></h5>
+
+  </div>
+  <div class="card-footer text-muted">
+    <button type="button" class="btn btn-outline-primary float-end">Open Task</button>
+  </div>
+</div>
+</div>`
+
+
 const saveChanges = () => {
     const taskData = {
         id: `${Date.now()}`,   //unique no for card id
@@ -8,5 +32,7 @@ const saveChanges = () => {
 
     };
 
-    console.log(taskData);
+    const createNewCard = newCard(taskData);
+
+    taskContainer.insertAdjacentHTML("beforeend", createNewCard);
 };
